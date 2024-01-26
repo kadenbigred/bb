ce367774c5d53b6fe17a183f4be601443f50acd7
kadenbigred <157423927+kadenbigred@users.noreply.github.com>
2024-01-26 16:43:30 -0500

    Create images.js

---
+++ src/images.js
@@ -0,0 +1,93 @@
+
+
+
+
+var jerboaImages = ['https://cdn.discordapp.com/attachments/693513623787536384/1196658905833553970/20211123_on_jerboa.jpg?ex=65b86e73&is=65a5f973&hm=f927aed97d94dc6b06a50ce92d1381d75488e44a74646122022a5740b0a7a120&', 
+    'https://cdn.discordapp.com/attachments/693513623787536384/1196659037870235668/image.png?ex=65b86e93&is=65a5f993&hm=0081f9d931199932531dba4273a3179a0536a98adb56d627112b25ba510a7954&', 
+    'https://cdn.discordapp.com/attachments/693513623787536384/1196659070606782565/jerboa_mos_022416051802.jpg?ex=65b86e9b&is=65a5f99b&hm=57f3bdb7731fdac4c5cc0361e7ec0a37461961f6d6ce23e05a436e00108da0ce&', 
+    'https://cdn.discordapp.com/attachments/693513623787536384/1196659135786254396/0c7a1d094b5bcee8a8caf1c570dcf9f0.png?ex=65b86eaa&is=65a5f9aa&hm=9e89f2b706207fa381cf7376cccefc9f07358fec28e8a2c604328512b3f15d15&', 
+    'https://cdn.discordapp.com/attachments/693513623787536384/1196659180212334675/01603493.webp?ex=65b86eb5&is=65a5f9b5&hm=24fda42c332317ef33cfe84d3f9c9469f42b2c15cfadb759eef9d60e14d812a0&', 
+    'https://cdn.discordapp.com/attachments/693513623787536384/1196659232297189477/wired_absurd-creatures-this-tiny-adorable-critter-is-half-kangaroo-half-velociraptor.jpg?ex=65b86ec1&is=65a5f9c1&hm=da219084a1fffea3c76f9a841146acff5000efed6e0b56bfda6398f8879341c2&', 
+    'https://cdn.discordapp.com/attachments/693513623787536384/1196659272206000150/digests_50645_digest-50645.png.jpg?ex=65b86ecb&is=65a5f9cb&hm=192d799e3267d8329254198d59fae1262aeda0fc66b302afcbca3ea5d59b5505&']
+
+    var caracalImages = ['https://cdn.discordapp.com/attachments/693513623787536384/1095910458248134737/FGDIzfAVcAIpqNu.jpg', 
+    'https://cdn.discordapp.com/attachments/693513623787536384/1095910491324420146/FF8RDr3VUAIkxMo.jpg', 
+    'https://cdn.discordapp.com/attachments/693513623787536384/1095910518071513199/FF5PaAXUcAA0TqN.jpg', 
+    'https://cdn.discordapp.com/attachments/693513623787536384/1095910540271943701/FF3YQvLVEAID4D2.jpg', 
+    'https://cdn.discordapp.com/attachments/693513623787536384/1095910564116566057/E-JctIOVUAEXiPD.jpg', 
+    'https://cdn.discordapp.com/attachments/693513623787536384/1095910595712274432/E8KNchdUcAMdnWo.jpg', 
+    'https://cdn.discordapp.com/attachments/693513623787536384/1095910625022050316/EwTxfOWUYAA_Rb2.jpg', 
+    'https://cdn.discordapp.com/attachments/693513623787536384/1095910663144087663/Caracal.webp', 
+    'https://cdn.discordapp.com/attachments/693513623787536384/1095910692848156773/231020361_2008629132623508_5218159801632802484_n.jpg', 
+    'https://cdn.discordapp.com/attachments/693513623787536384/1095910724330586182/52535667411_2ae4b1c2e9_c.jpg', 
+    'https://cdn.discordapp.com/attachments/693513623787536384/1095910755540402237/Sassy-Caracal-2016.webp', 
+    'https://cdn.discordapp.com/attachments/693513623787536384/1095910815883853905/81297d55-d18a-457e-9b88-bf603f7605d0-cats-6-1.webp']
+
+    var hyenaImages = ['https://cdn.discordapp.com/attachments/1095022465199120466/1196656309618086069/Z.png', 
+    'https://cdn.discordapp.com/attachments/1095022465199120466/1196656365570109450/ZVMPI4WGMYI6TCHRIBWI6S7HAY.png', 
+    'https://cdn.discordapp.com/attachments/1095022465199120466/1196656398998700083/tiny-baby-spotted-hyena-cub-600nw-1744105886.png', 
+    'https://cdn.discordapp.com/attachments/1095022465199120466/1196656453965062205/hgzkyg7g4yf21.png', 
+    'https://cdn.discordapp.com/attachments/1095022465199120466/1196656453965062205/hgzkyg7g4yf21.png', 
+    'https://cdn.discordapp.com/attachments/1095022465199120466/1196656512739856395/baby-hyena.png']
+
+    var hogImages = ['https://cdn.discordapp.com/attachments/693513623787536384/1095904363177844806/RedRiverHog_0084.webp', 
+    'https://media.discordapp.net/attachments/693513623787536384/1095904426432139315/image33.jpeg?width=824&height=676', 
+    'https://cdn.discordapp.com/attachments/693513623787536384/1095904490319794227/Hog-Red-River-Piglet-JP.jpg', 
+    'https://media.discordapp.net/attachments/693513623787536384/1095905233231683704/redriverhog-1-gallery.jpg', 
+    'https://media.discordapp.net/attachments/693513623787536384/1095908781013074100/RedRiverHog02-1.jpg', 
+    'https://cdn.discordapp.com/attachments/693513623787536384/1095908852156866602/red-river-hog-dsc_7903.jpg', 
+    'https://cdn.discordapp.com/attachments/693513623787536384/1095908899418292265/RedRiverHog.jpg', 
+    'https://cdn.discordapp.com/attachments/693513623787536384/1095908937867468840/red-river-hog.jpg']
+
+    var foxImages = ['https://cdn.discordapp.com/attachments/693513623787536384/1199482373516034098/FFJKon4WUAUXBL9.jpg',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199482936584577126/20240123_073531.jpg',
+    'https://media.discordapp.net/attachments/693513623787536384/1199490917325865051/GettyImages-671702530-d9032ee.jpg',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199491428770906142/GEftgGxbQAAQUSA.jpg',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199491631859126293/GEQ57P4aoAAQ7-2.jpg',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199491769616830544/GEOVIAubAAAQfl4.jpg',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199491957639102514/GEHdraqbsAAwjz_.jpg',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199492184915853484/GDQvuc_aQAAVsds.jpg',
+    'https://media.discordapp.net/attachments/693513623787536384/1199492407490793572/GDMqzKibYAArB26.jpg',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199492632439685180/GEf7JMLWYAAfSdm.jpg',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199492778435039302/GEebA1oXEAAxLVB.jpg',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199492944080666694/GEdIna_XMAAQ5LC.jpg',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199493073319772340/GEcRrx-WEAAttGE.jpg',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199493248666832966/GEaj0hyWEAAbuW-.jpg',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199493381286539264/GEZs47rWMAApft2.jpg',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199493516515102791/GEYMwkCXwAAsB7K.jpg',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199493955222523955/GEW6XISWUAAjCGE.jpg',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199494055709638656/GEWe5UJWYAAWZj1.jpg',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199494265231908954/GEVn9reWAAEcTWS.png',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199494443892486215/GETsXlJXMAAvQ14.jpg',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199494568891138108/GESZ-GJWkAAf1R5.jpg',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199494682430943323/GERwxYJXEAANLrt.jpg',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199495069254815776/IMG_1191.jpeg',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199495158589304895/IMG_8698.jpg',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199495342551486634/GELwQAeXAAAfAt8.jpg',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199495504774574190/GEKCYwKXoAAJ-e6.jpg',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199495749684179015/GECGRPCWEAAHleW.jpg',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199495857385521272/GEBdEgOXEAEIZiR.jpg',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199496010301440020/GD-4RoFWYAA6ykK.jpg',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199496479212056656/yay-foxxo-stares-into-your-soul-v0-rkcr2jwyzb3c1.webp',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199496669801234483/image.png',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199496792337829958/image.png',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199497248296407050/yay-foxxo-encounters-a-baby-foxxo-v0-pd98xgvefiob1.webp',
+    'https://tenor.com/view/fox-gif-25805627',
+    'https://tenor.com/view/fox-you-have-alerted-the-fox-foxes-gif-4003420250280337461',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199497754276274236/two_feet.gif',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199497924850237470/yay-foxxo-does-a-blep-while-sleeping-v0-g0qi9kr9igkb1.webp',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199498014369263777/yay-foxxo-wants-you-to-waste-98-32-kb-of-internet-data-v0-lbdwre595bkb1.webp',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199498452250415205/fox.mp4',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199498828185882744/fox.mp4',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199499227450048512/their-human-costumes-are-almost-becoming-too-convincing-but-v0-ws3bmhx9izbb1.webp',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199499616832458884/fox.mp4',
+    'https://cdn.discordapp.com/attachments/693513623787536384/1199499815113994240/0arih25bvr2b1.webp']
+    
+
+    module.exports = {
+        jerboaImages,
+        caracalImages,
+        hyenaImages,
+        hogImages,
+        foxImages,
+      };
